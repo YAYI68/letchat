fe73da62c5fe8232bc79d55d7c959f1cb9aa8a79
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-25 04:34:31 -0700

    commit

--- src/components/ChatContent.js
+++ src/components/ChatContent.js
@@ -4,7 +4,7 @@ export const ChatContent = () => {
   return (
 <div className='w-[60%] h-full relative'>
     <div className='absolute h-full w-full flex flex-col '>     
-    <div className='h-[3rem] w-full bg-gray-200 dark:bg-gray-700 '>
+    <div className='h-[4rem] w-full bg-gray-200 dark:bg-gray-700 '>
         profile
     </div>
     </div>
---
+++ src/components/ChatListHeader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ChatListHeader = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
