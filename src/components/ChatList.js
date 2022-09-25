6da9105cc4591d99ed5f3237edc3509f4b0d4e8d
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-25 04:42:30 -0700

    commit

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -1,24 +1,12 @@
 import React from 'react'
-import { FaUserPlus } from "react-icons/fa";
-import { HiOutlineMenu } from "react-icons/hi";
+import { ChatListHeader } from './ChatListHeader'
+
 
 export const ChatList = () => {
   return (
            <div className='w-[40%] shadow-md border-r-1 relative border-r-gray-400 h-full '>
            <div className='h-full absolute w-full flex flex-col dark:bg-main-dark-bg bg-white'> 
-            <div className='h-[4rem] absolute flex  justify-between w-full bg-gray-200 px-3 py-[.15rem] dark:bg-gray-700 '>
-                <div className='cursor-pointer h-full w-[4.5rem] rounded-full overflow-hidden'>
-                  <img src="./images/avatar.png" alt="" className='h-full w-full ' />
-                 </div> 
-                 <div className='w-[35%] h-full p-2 justify-between flex items-center'>
-                    <button title='Contacts' type="button" className='h-[2.5rem] w-[2.5rem] rounded-full hover:bg-slate-100 flex items-center justify-center'>
-                        <FaUserPlus className='h-[1.5rem] w-[1.5rem]'/>
-                    </button>
-                    <button type="button" className='h-[2.5rem] w-[2.5rem] rounded-full hover:bg-slate-100 flex items-center justify-center'>
-                        <HiOutlineMenu  className='h-[1.5rem] w-[1.5rem]'/>
-                    </button>
-                 </div>
-            </div>
+             <ChatListHeader/>
            </div>
             </div>
   )
